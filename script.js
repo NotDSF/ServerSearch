04ce2580edfeaa7daeee79754cff54591bc139b8
NotDSF <67711164+NotDSF@users.noreply.github.com>
2022-01-29 22:37:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         RoSearcher
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Find empty Roblox servers at the click of a button.
+// @version      0.5
+// @description  Find roblox servers at the click of a button.
 // @author       https://github.com/NotDSF
 // @match        https://www.roblox.com/games/*
-// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @icon         https://www.google.com/s2/favicons?domain=roblox.com
 // @grant        none
 // ==/UserScript==
 
@@ -15,7 +15,7 @@
     let Cookie = document.cookie.match(/rosearch=(?<json>{.*?});/);
     let Options = Cookie?.groups.json ? JSON.parse(Cookie?.groups.json) : {};
     
-    let Version = "0.4";
+    let Version = "0.5";
     let GameID = document.URL.match(/games\/(\d+)\//)[1];
     let GameOptions = Options[GameID] || {};
 
@@ -33,12 +33,10 @@
     function SaveOptions() {
         let Status     = document.getElementById("server-status");
         let MaxPing    = document.getElementById("ping-box").value;
-        let Iterator   = document.getElementById("iterator-box").value;
         let MaxPlayers = document.getElementById("player-box").value;
         let CookieData = {};
         
         if (MaxPing.length) CookieData.MaxPing = MaxPing;
-        if (Iterator.length) CookieData.Iterator = Iterator;
         if (MaxPlayers.length) CookieData.MaxPlayers = MaxPlayers;
 
         let StatusBackup = Status.innerHTML;
@@ -63,74 +61,6 @@
             alert("RoSearcher Error: " + er.toString());
             throw er;
         });
-
-    async function main() {
-        if (!confirm("Are you sure you want to find an empty server?")) return;
-
-        let Status      = document.getElementById("server-status");
-        let MaxPingB    = document.getElementById("ping-box");
-        let IteratorB   = document.getElementById("iterator-box");
-        let MaxPlayersB = document.getElementById("player-box");
-
-        let MaxPing     = MaxPingB.value.length ? parseInt(MaxPingB.value) : 300;
-        let Iterator    = IteratorB.value.length ? parseInt(IteratorB.value) : 30;
-        let MaxPlayers  = MaxPlayersB.value.length ? parseInt(MaxPlayersB.value) : 4;
-
-        let Index = 0;
-        let LowestServer;
-
-        Status.innerHTML = "Looking for servers...";
-
-        while (true) {
-            if (LowestServer && LowestServer.Len <= MaxPlayers && LowestServer.Ping <= MaxPing) {
-                Status.innerHTML = `Found server! Players: ${LowestServer.Len} Ping: ${LowestServer.Ping} Index: ${Index}. Joining...`;
-                eval(LowestServer.JoinScript);
-                break;
-            }
-
-            let Servers = await GetServers(Index)
-                .catch(er => {
-                    throw er;
-                });
-
-            if (!Servers.Collection.length) {
-                if (LowestServer && Iterator === 10) {
-                    if (LowestServer.Len > MaxPlayers) alert(`We couldn't find a server with ${MaxPlayers} players or below! (Try editing your options)`);
-                    if (LowestServer.Ping > MaxPing) alert(`We couldn't find a server with ${MaxPing} ping or below! (Try editing your options)`);
-
-                    Status.innerHTML = `Found server! Players: ${LowestServer.Len} Ping: ${LowestServer.Ping} Index: ${Index}. Joining...`;
-                    eval(LowestServer.JoinScript);
-                    break;
-                }
-
-                if (LowestServer) {
-                    Iterator = 10;
-                    Index -= (Index > (Iterator * 2)) ? (Iterator * 2) : Iterator; // Just incase
-                    continue;
-                }
-
-                alert("Sorry we couldn't find a server!");
-                Status.innerHTML = "";
-                break;
-            }
-            
-            for (let Server of Servers.Collection) {
-                let PlayerLen = Server.CurrentPlayers.length;
-
-                if (!LowestServer) {
-                    LowestServer = { Len: PlayerLen, Ping: Server.Ping, JoinLink: Server.JoinScript }
-                    continue;
-                }
-
-                if (PlayerLen < LowestServer.Len) {
-                    LowestServer = { Len: PlayerLen, Ping: Server.Ping, JoinScript: Server.JoinScript }
-                }
-            }
-
-            Index += Iterator;
-            Status.innerHTML = `Index: ${Index}, Lowest Server: ${LowestServer.Len}, Ping: ${LowestServer.Ping}...`;
-        }
-    }
 
     let GameInstances = document.getElementById("game-instances");
     let ParentDiv     = document.createElement("div");
@@ -164,15 +94,40 @@
     maxPlayer.placeholder = "Max Players (Leave Blank For Default)";
     maxPlayer.innerHTML = GameOptions.MaxPlayers || "";
     maxPlayer.id = "player-box";
-
-    let Iterator = document.createElement("textarea");
-    Iterator.className = "dialog-input ng-valid ng-isolate-scope ng-touched ng-not-empty ng-dirty ng-valid-parse";
-    Iterator.rows = 1;
-    Iterator.style = "overflow: hidden; overflow-wrap: break-word; resize: none; height: 30px; margin-bottom: 5px; display: inherit; border-radius: 3px; width: 330px;";
-    Iterator.placeholder = "Iterator (Leave Blank For Default)";
-    Iterator.innerHTML = GameOptions.Iterator || "";
-    Iterator.id = "iterator-box";
     
+    let status = document.createElement("span");
+    status.className = "section-content-off";
+    status.id = "server-status";
+    status.style = "display: inherit;";
+
+    async function main() {
+        if (!confirm("Are you sure you want to find the server?")) return;
+
+        let Stats       = document.getElementsByClassName("game-stat game-stat-width");
+        let MaxPing     = pingbox.value.length ? parseInt(pingbox.value) : 300;
+        let MaxPlayers  = maxPlayer.value.length ? parseInt(maxPlayer.value) : 4;
+        let MaxServerP  = parseInt(Stats[5].children[1].innerText);
+        let ActiveP     = parseInt(Stats[0].children[1].innerText.replace(",", ""));
+        let Iterator    = Math.floor(ActiveP / (MaxServerP * 9));
+        
+        console.log(MaxPing, MaxPlayers, Iterator);
+        status.innerHTML = "Looking for servers...";
+
+        while (true) {
+            let Servers = await GetServers(Iterator);
+            let Matched = Servers.Collection.filter((server) => server.CurrentPlayers.length <= MaxPlayers && server.Ping <= MaxPing).shift();
+
+            if (Matched) {
+                status.innerHTML = `Found server! Players: ${Matched.CurrentPlayers.length} Ping: ${Matched.Ping} Index: ${Iterator}`;
+                eval(Matched.JoinScript);
+                break;
+            }
+
+            status.innerHTML = `Index: ${Iterator}.`;
+            Iterator += Math.floor(ActiveP / (MaxServerP * 9));
+        }
+    }
+
     let fakeButton = document.createElement("span");
     fakeButton.className = "btn-secondary-md btn-more";
     fakeButton.innerHTML = "Find Empty Server";
@@ -185,10 +140,5 @@
     fakeButton2.onclick = SaveOptions;
     fakeButton2.style = "display: inherit; width: min-content; margin-bottom: 5px;";
 
-    let status = document.createElement("span");
-    status.className = "section-content-off";
-    status.id = "server-status";
-    status.style = "display: inherit;";
-
-    ParentDiv.prepend(ContainerHead, maxPlayer, pingbox, Iterator, fakeButton2, fakeButton, status);
+    ParentDiv.prepend(ContainerHead, maxPlayer, pingbox, fakeButton2, fakeButton, status);
 })();
